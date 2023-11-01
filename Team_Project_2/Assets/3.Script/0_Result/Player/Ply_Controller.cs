129bfa07a4124e2294cd3c6044fc8755a84415e5
minki0324 <147981201+minki0324@users.noreply.github.com>
2023-11-01 15:39:27 +0900

    Update Ply_Controller.cs

--- Team_Project_2/Assets/3.Script/0_Result/Player/Ply_Controller.cs
+++ Team_Project_2/Assets/3.Script/0_Result/Player/Ply_Controller.cs
@@ -8,18 +8,19 @@ public class Ply_Controller : MonoBehaviour
         1. 숫자키 입력으로 병사 소환
         2. 단축키 입력으로 병사들 진영 배치
     */
-    
+
     public enum Mode
     {
-        Follow = 1,      //나를 따르라~
+        Follow = 1,      //나를 따르라~ (플레이어가 멈추면 대열갖추기)
         Attack,    //돌격하라~
-        Assemble    //그자리에 멈추기 & 적이 다가오면 방어모드
+        Stop    //멈춰라~~
     }
 
     public Mode CurrentMode;
 
     //다른 클래스 객체=====================
     private Minion_Controller[] minionController;
+
     private Following following;
 
     //플레이어 정보========================
@@ -50,10 +51,30 @@ public class Ply_Controller : MonoBehaviour
     private List<GameObject> nearestMinion_List = new List<GameObject>();
 
 
+    //변수추가 이서영
+
+    public bool isOperateStop = false;
+
+    public bool isOperateFollow = true;
+
+
+    public bool isDead { get; private set; }
+
+
+
+    //추가 이서영
+    [SerializeField]
+    private Animator animator;
+
+    public bool isPlay_AttackOrder = false;
+    public bool isPlay_FollowOrder = false;
+    public bool isPlay_StopOrder = false;
+
     private void Awake()
     {
         following = GetComponent<Following>();
         CurrentMode = Mode.Follow;
+
     }
 
     private void Update()
@@ -63,31 +84,63 @@ public class Ply_Controller : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Q))
         {
             Debug.Log("나를 따르라~~");
-            
+
+            if (!isPlay_FollowOrder)
+            {
+                animator.SetTrigger("FollowOrder");
+                isPlay_FollowOrder = true;
+            }
             CurrentMode = Mode.Follow;
+            isOperateFollow = true;
         }
-            
+
         if (Input.GetKeyDown(KeyCode.E))
         {
             Debug.Log("돌격하라~~");
+            if (!isPlay_AttackOrder)
+            {
+                animator.SetTrigger("AttackOrder");
+                isPlay_AttackOrder = true;
+            }
             CurrentMode = Mode.Attack;
         }
 
+
+
         if (Input.GetKeyDown(KeyCode.R))
         {
-            Debug.Log("모여라~~");
-            CurrentMode = Mode.Assemble;
+            Debug.Log("멈춰라~~");
+
+            if (!isPlay_StopOrder)
+            {
+                animator.SetTrigger("StopOrder");
+                isPlay_StopOrder = true;
+            }
+
+
+            CurrentMode = Mode.Stop;
+            isOperateStop = true;
+
+            isOperateFollow = false;
+
+
+
         }
+
+
+
+
+
     }
 
     private void Spawn_Solider()
     {
-        if(Current_MinionCount < Max_MinionCount)
+        if (Current_MinionCount < Max_MinionCount)
         {
-            if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Alpha3))
+            if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Alpha3))
             {
                 int selectedNumber = -1; // 기본값 설정
-                
+
 
                 if (Input.GetKeyDown(KeyCode.Alpha1))
                 {
@@ -102,14 +155,15 @@ public class Ply_Controller : MonoBehaviour
                     selectedNumber = 3;
                 }
 
-                if(selectedNumber != -1)
+                if (selectedNumber != -1)
                 {
+
                     switch (selectedNumber)
                     {
                         case 1:
                             Debug.Log("1눌림");
                             Human_num = 0;
-                            if(GameManager.instance.Gold > 15)
+                            if (GameManager.instance.Gold > 15)
                             {
                                 Init_Solider(Human_num);
                             }
@@ -148,6 +202,9 @@ public class Ply_Controller : MonoBehaviour
                             }
                             break;
                     }
+
+
+                    StartCoroutine(following.Mode_Follow_co());
                 }
             }
         }
@@ -161,10 +218,10 @@ public class Ply_Controller : MonoBehaviour
         Minion = Instantiate(Minion_Prefabs[Human_num], Spawner.position, Quaternion.identity);
         //미니언 생성 위치는 나중에 점령지(Spawner)위치로 바꾸기 
 
-        Minion_Controller minionController = Minion.AddComponent<Minion_Controller>();
+        Minion_Controller minionController = Minion.GetComponent<Minion_Controller>();
 
         GameManager.instance.Gold -= (15 + (Human_num * 5));
-        Minion.transform.SetParent(transform);
+        //Minion.transform.SetParent(transform);
         Minions_List.Add(Minion);
         Current_MinionCount++;
     }
