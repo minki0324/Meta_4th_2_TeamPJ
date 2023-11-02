ea2fbb2f54fb283ea29ca470964bd65bd20fb8a3
Kwan <janyouns@gmail.com>
2023-11-02 16:46:40 +0900

    1102 문열기 및 미니맵에 표시

--- Team_Project_2/Assets/3.Script/0_Result/Player/Ply_Interaction.cs
+++ Team_Project_2/Assets/3.Script/0_Result/Player/Ply_Interaction.cs
@@ -11,34 +11,41 @@ public class Ply_Interaction : MonoBehaviour
     // 1. 점령 후 깃발 색 변경
     // 2. 점령 후 포인트가 속도 변경
     // 3. 주변 유닛 수에 따른 점령 슬라이더 변경
+    // 하루종일 쳐자서 죄송합니다,,
 
-
-   [SerializeField] private Occupation occupation;
+    [SerializeField] private Occupation occupation; // 점령 스크립트
+    [SerializeField] private DoorInter Doorinter;   // 문 열기 스크립트
+    [SerializeField] private Text Doorui; 
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Flag"))
+        if (other.gameObject.CompareTag("Flag"))
         {
             occupation.ObjEnable(true);
 
             StopCoroutine(occupation.UnOccu_co());
             StartCoroutine(occupation.Occu_co());
         }
+        if (other.gameObject.CompareTag("Door"))
+        {
+            Doorui.gameObject.SetActive(true);
+            StartCoroutine(Doorinter.OpenDoor_co());
+        }
     }
     private void OnTriggerExit(Collider other)
     {
-        if(other.gameObject.CompareTag("Flag"))
+        if (other.gameObject.CompareTag("Flag"))
         {
-            
+
             occupation.ObjEnable(false);
             StopCoroutine(occupation.Occu_co());
             StartCoroutine(occupation.UnOccu_co());
         }
+        if (other.gameObject.CompareTag("Door"))
+        {
+            Doorui.gameObject.SetActive(false); 
+            StopCoroutine(Doorinter.OpenDoor_co());
+        }
     }
-    
-
-
-    
-    
     
 }
