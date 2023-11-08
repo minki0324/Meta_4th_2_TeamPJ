e7e21321ab2a8a9142ec4c67017cd813d7f3a213
이서영 <89364144+Won9012@users.noreply.github.com>
2023-11-08 17:58:38 +0900

    ㅁㄴㅇㄹ

---
+++ Team_Project_2/Assets/3.Script/Seoyoung/SkyBox_Controller.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SkyBox_Controller : MonoBehaviour
+{
+
+    public Material night_M;
+    public Material evening_M;
+    public Material afternoon_M;
+
+
+
+
+    public Material current_material;
+
+    float duration = 3; //3초마다
+    float smoothness = 0.1f; //부드러움? 색깔 변화 텀
+
+
+    void Start()
+    {
+        current_material = afternoon_M;
+        gameObject.GetComponent<Skybox>().material = current_material;
+ 
+
+        StartCoroutine(Update_SkyBox_Night());
+    }
+
+
+
+    private void Update()
+    {
+        //if (GameManager.instance.currentTime >= 3f)
+        //{
+        //    //this.gameObject.GetComponent<Skybox>().material = night_M;
+        //    StartCoroutine(Update_SkyBox_Night());
+        //}
+
+
+
+    }
+
+
+
+
+
+
+    private IEnumerator Update_SkyBox_Night()
+    {
+       
+        float progress; //매개변수 대용
+
+        float increment = smoothness / duration; //적용할 변경수준
+
+        progress = 0;
+        while (true)
+        {
+            Debug.Log("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+            //current_material.Lerp(afternoon_M, night_M, progress);
+            gameObject.GetComponent<Skybox>().material.Lerp(afternoon_M, night_M, progress);
+           // Debug.Log(this.gameObject.GetComponent<Skybox>().material);
+            progress += increment;
+            yield return new WaitForSeconds(smoothness);        //컬렉션 데이터를 하나씩 리턴
+        }
+       
+    }
+}
