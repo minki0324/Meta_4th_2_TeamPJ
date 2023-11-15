a6d49be75928681a5d42177bc0e4a01ef184ea8f
이서영 <89364144+Won9012@users.noreply.github.com>
2023-11-15 18:13:47 +0900

    dd

--- Team_Project_2/Assets/3.Script/Minki/Test2.cs
+++ Team_Project_2/Assets/3.Script/Minki/Test2.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
 using Pathfinding.Util;
+using System.Linq;
 
 public class Test2 : MonoBehaviour
 {
     private Ply_Controller player_Con;
-    [SerializeField] private Transform[] Formation_Pos;
+    [SerializeField] private Transform[] Parents_Pos;
+    [SerializeField] private Formation_Count[] Count;
 
     private void Awake()
     {
@@ -18,17 +21,22 @@ public class Test2 : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.U))
         {
-            Following();
+            Following(100);
         }
         else if(Input.GetKeyDown(KeyCode.I))
         {
-            SetFormation();
+            SetFormation(100);
         }
     }
 
-    private void Following()
+    private void Following(int scanRange)
     {
-        for(int i = 0; i < player_Con.UnitList_List.Count; i++)
+        for(int i = 0; i < Count.Length; i++)
+        {
+            Count[i].Count = 0;
+        }
+
+        for (int i = 0; i < player_Con.UnitList_List.Count; i++)
         {
             GameObject unit = player_Con.UnitList_List[i];
             Animator anim = unit.GetComponent<Animator>();
@@ -36,43 +44,130 @@ public class Test2 : MonoBehaviour
         }
     }
 
-    private void SetFormation()
+    private void SetFormation(int scanRange)
     {
-        for (int i = 0; i < player_Con.UnitList_List.Count; i++)
+
+        List<GameObject> unitList = Scan_Pos(scanRange);
+
+        foreach (GameObject unit in unitList)
         {
-            GameObject unit = player_Con.UnitList_List[i];
-            Animator anim = unit.GetComponent<Animator>();
-            unit.GetComponent<AIDestinationSetter>().target = Formation_Pos[i];
+            Transform[] sortedParents = GetSortedParentsByWeight(unit);
+            Transform selectedParent = null;
+
+            // 적절한 부모 포지션 찾기
+            foreach (Transform parent in sortedParents)
+            {
+                if (parent.childCount != parent.GetComponent<Formation_Count>().Count)
+                {
+                    selectedParent = parent;
+                    break;
+                }
+            }
+
+            // 유닛의 타겟 설정
+            if (selectedParent != null)
+            {
+                unit.GetComponent<AIDestinationSetter>().target = selectedParent.GetChild(selectedParent.GetComponent<Formation_Count>().Count).transform;
+                selectedParent.GetComponent<Formation_Count>().Count++;
+            }
+            else
+            {
+                Debug.LogError("No available position for unit: " + unit.name);
+                // 모든 부모 위치가 꽉 찼을 경우의 처리
+            }
         }
     }
 
-    private List<GameObject> Scan(float scanRange)
+    private List<GameObject> Scan_Unit(float scanRange)
     {
         RaycastHit[] allHits = Physics.SphereCastAll(transform.position, scanRange, Vector3.forward, 0);
-        GameObject nearestTarget = null;
-        List<GameObject> nearList = null;
-        if (allHits != null)
+
+        List<GameObject> unitList = new List<GameObject>();
+
+        foreach (RaycastHit hit in allHits)
         {
-            foreach (RaycastHit hit in allHits)
+            GameObject hitObject = GetNearestTarget(allHits).gameObject;
+            unitList.Add(hitObject);
+        }
+
+        return unitList;
+    }
+
+    private List<GameObject> Scan_Pos(float scanRange)
+    {
+        RaycastHit[] allHits = Physics.SphereCastAll(transform.position, scanRange, Vector3.forward, 0);
+        HashSet<GameObject> uniqueTargets = new HashSet<GameObject>();
+
+        foreach (RaycastHit hit in allHits)
+        {
+            GameObject hitObject = hit.transform.gameObject;
+            if (hitObject.layer == gameObject.layer && !hitObject.CompareTag("Player") && !hitObject.CompareTag("Leader") && !hitObject.CompareTag("Base"))
             {
-                if (hit.transform.gameObject.layer == gameObject.layer)
+                if (!uniqueTargets.Contains(hitObject))
                 {
-                    nearestTarget = GetNearestTarget(allHits);
-                    nearList.Add(nearestTarget);
+                    uniqueTargets.Add(hitObject);
                 }
             }
         }
-        return nearList;
+
+        return new List<GameObject>(uniqueTargets);
+    }
+   
+    private Transform[] GetSortedParentsByWeight(GameObject unit)
+    {
+        Vector3 currentPosition = unit.transform.position;
+        List<(Transform transform, float weightedDistance)> weightedParents = new List<(Transform, float)>();
+
+        foreach (Transform parent in Parents_Pos)
+        {
+            float distance = Vector3.Distance(currentPosition, parent.position);
+            float weight = GetWeightForParent(parent);
+
+            weightedParents.Add((parent, distance * weight));
+        }
+
+        // 가중치된 거리에 따라 정렬
+        weightedParents.Sort((a, b) => a.weightedDistance.CompareTo(b.weightedDistance));
+
+        // 정렬된 트랜스폼 배열 추출
+        return weightedParents.Select(item => item.transform).ToArray();
     }
 
-    private GameObject GetNearestTarget(RaycastHit[] hits)
+    private float GetWeightForParent(Transform parent)
     {
-        GameObject nearest = null;
+        int index = Array.IndexOf(Parents_Pos, parent);
+        float baseWeight = 1.0f; // 기본 가중치
+
+        if (parent.childCount == parent.GetComponent<Formation_Count>().Count)
+        {
+            return float.MaxValue; // 풀일 경우 매우 높은 가중치를 설정하여 우선순위를 낮춤
+        }
+        else if (index >= 0 && index <= 3)
+        {
+            return baseWeight / 2f; // 1부터 4번 인덱스에 대한 낮은 가중치
+        }
+        else if (index == 4)
+        {
+            return baseWeight / 1.2f; // 5번 인덱스에 대한 낮은 가중치
+        }
+        else if (index >= 5 && index <= 6)
+        {
+            return baseWeight / 1.7f; // 6에서 7번 인덱스에 대한 낮은 가중치
+        }
+        return baseWeight; // 나머지는 기본 가중치
+    }
+
+    private Transform GetNearestTarget(RaycastHit[] hits)
+    {
+        Transform nearest = null;
         float closestDistance = float.MaxValue;
 
         foreach (RaycastHit hit in hits)
         {
-            if (hit.transform.CompareTag("SpawnPoint"))
+            if (hit.transform.CompareTag("SpawnPoint") &&
+                !hit.transform.CompareTag("Player") && 
+                !hit.transform.CompareTag("Leader") &&
+                !hit.transform.CompareTag("Base"))
             {
                 continue;
             }
@@ -82,7 +177,7 @@ public class Test2 : MonoBehaviour
             if (distance < closestDistance && !hit.transform.CompareTag("SpawnPoint"))
             {
                 closestDistance = distance;
-                nearest = hit.transform.gameObject;
+                nearest = hit.transform;
             }
         }
 
