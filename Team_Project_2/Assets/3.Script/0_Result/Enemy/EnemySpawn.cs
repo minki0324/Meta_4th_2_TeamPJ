356073a0ae27143e43371fa3ed59fff3bf8e0493
Nangdi <129065548+Nangdi@users.noreply.github.com>
2023-11-18 18:15:50 +0900

    잘못머지 조치
    오류뜨는부분삭제

--- Team_Project_2/Assets/3.Script/0_Result/Enemy/EnemySpawn.cs
+++ Team_Project_2/Assets/3.Script/0_Result/Enemy/EnemySpawn.cs
@@ -177,22 +177,6 @@ public class EnemySpawn : MonoBehaviour
         Unit_Information currentUnit = GameManager.instance.units[leaderAI.unitValue];
         GameObject newUnit = Instantiate(currentUnit.unitObject, SpawnPoint[SpawnIndex].position, Quaternion.identity);
         SetLayerRecursively(newUnit, leaderAI.gameObject.layer);
-        switch (targetLeader.gameObject.layer)
-        {
-            case 7:
-                ColorManager.instance.RecursiveSearchAndSetUnit(newUnit.transform, GameManager.instance.T1_Color);
-                Upgrade_Set(0, soilder_Con);
-                break;
-            case 8:
-                ColorManager.instance.RecursiveSearchAndSetUnit(newUnit.transform, GameManager.instance.T2_Color);
-                Upgrade_Set(1, soilder_Con);
-                break;
-            case 9:
-                ColorManager.instance.RecursiveSearchAndSetUnit(newUnit.transform, GameManager.instance.T3_Color);
-                Upgrade_Set(2, soilder_Con);
-                break;
-
-        }
         
         Soilder_Controller soilder_Con = newUnit.GetComponent<Soilder_Controller>();
         soilder_Con.infodata = currentUnit;
